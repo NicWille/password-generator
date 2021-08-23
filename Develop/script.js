01d22a509e20ddc74a5fd38d4aaf20444b278fb2
NicWille <nic.wille1020@yahoo.com>
2021-08-23 11:30:57 -0700

    library creator func

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,51 @@
-// Assignment code here
-var amount = document.querySelector("#amount")
+// references to the # elements
+var amount = document.querySelector("#amount");
 var lowercase = document.querySelector("#lowercase");
 var uppercase = document.querySelector("#uppercase");
 var number = document.querySelector("#number");
 var special = document.querySelector("#special");
-
-
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var amountNumber = +amount.value;
+
+function libraryCreator() {
+
+  var library = '';
+
+  var lowerBoolean = lowercase.checked;
+  var upperBoolean = uppercase.checked;
+  var numBoolean = number.checked;
+  var specialBoolean = special.checked;
+
+  var digitList = '0123456789';
+  var lowerList = 'abcdefghijklmnopqrstuvwxyz';
+  var upperList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var specialList = '!@#$%&*?';
+
+  if (numBoolean) library = library + digitList
+  if (lowerBoolean) library = library + lowerList
+  if (upperBoolean) library = library + upperList
+  if (specialBoolean) library = library + specialList
+
+  return library 
+}
 
 // Write password to the #password input
 function writePassword() {
 
-console.log("button pressed")
-console.log(amount.value)
-console.log(lowercase.checked)
+  var library = libraryCreator()
+  console.log(library)
 
   // var password = generatePassword();
 //   var passwordText = document.querySelector("#password");
 
 //   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+
+
+
+
+
+// event listener to generate button
 generateBtn.addEventListener("click", writePassword);
