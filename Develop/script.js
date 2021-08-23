4c67047f61fc220991e709572671534d09db2ba0
NicWille <nic.wille1020@yahoo.com>
2021-08-22 19:56:33 -0700

    got values from checkboxs

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,11 @@
 // Assignment code here
+var amount = document.querySelector("#amount")
+var lowercase = document.querySelector("#lowercase");
+var uppercase = document.querySelector("#uppercase");
+var number = document.querySelector("#number");
+var special = document.querySelector("#special");
+
+
 
 
 // Get references to the #generate element
@@ -6,10 +13,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+console.log("button pressed")
+console.log(amount.value)
+console.log(lowercase.checked)
+
+  // var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
 
 }
 
